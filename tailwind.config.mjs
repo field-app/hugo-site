b16b1ecf9beca377bfb2c9cfc4245a18a144d713
chrishollandaise <chrishollandaise@gmail.com>
2024-03-19 17:25:12 -0700

    basic structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'field-black': '#000000',
+				'field-white': '#FFFFFF',
+				'field-darkgray': '#1E1E1E',
+				'field-gray': '#98999A',
+				'field-lightgray': '#F2F2F2',
+				'field-green': '#1BD760',
+			},
+		},
 	},
 	plugins: [],
 }
